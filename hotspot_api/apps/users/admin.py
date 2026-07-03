cd4f293592004ad8ddb7d823186726ba10627954
xxh2333 <2633681826@qq.com>
2026-07-03 10:07:13 +0800

    django框架

---
+++ hotspot_api/apps/users/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'phone', 'nickname', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('is_active', 'is_staff', 'date_joined')
+    search_fields = ('username', 'email', 'phone', 'nickname')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('个人信息', {'fields': ('email', 'phone', 'avatar', 'nickname')}),
+        ('权限', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('重要日期', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'phone', 'password1', 'password2', 'is_active', 'is_staff'),
+        }),
+    )
---
+++ hotspot_api/apps/users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=11, blank=True, null=True, unique=True, verbose_name='手机号')
+    avatar = models.URLField(blank=True, null=True, verbose_name='头像')
+    nickname = models.CharField(max_length=50, blank=True, null=True, verbose_name='昵称')
+    is_active = models.BooleanField(default=True, verbose_name='是否激活')
+    
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = '用户'
+    
+    def __str__(self):
+        return self.username
